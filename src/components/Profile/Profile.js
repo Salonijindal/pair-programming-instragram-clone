637da07af6d035dbfafffe5e53366e22aaa7bdc4
Saloni Jindal <salonijindal@MacBook-Pro.ht.home>
2022-07-14 19:58:14 -0400

    Add partials folder and variables folder. Profile components is retriving from user data base

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,28 @@
 import "./Profile.scss";
 import React from "react";
+import users from "../../data/users";
+import saloniImage from "../../assets/images/saloni-jindal.jpg";
+import ProfileIcon from "../ProfileIcon/ProfileIcon";
+const Profile = (props) => {
+  const {
+    username,
+    caption,
+    urlText,
+    iconSize,
+    image,
+    captionSize,
+    storyBorder,
+    hideAccountName,
+  } = props;
 
-const Profile = () => {
-  return <div>Profile</div>;
+  let accountName = username
+    ? username
+    : users[Math.floor(Math.random() * users.length)].username;
+  return (
+    <div className="profile">
+      <ProfileIcon />
+    </div>
+  );
 };
 
 export default Profile;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,28 @@
 import React from "react";
 import Sticky from "react-sticky-el";
 import Profile from "../Profile/Profile";
+import Suggestions from "../Suggestions/Suggestions";
+import Footer from "../Footer/Footer";
+import saloniImage from "../../assets/images/saloni-jindal.jpg";
 
 function Sidebar() {
-  return <div>Hello World</div>;
+  return (
+    <div className="sidebar">
+      <Sticky topOffset={-80}>
+        <div className="sidebar">
+          <Profile
+            username="Saloni Jindal"
+            caption="Just love it!"
+            urlText="Switch"
+            iconSize="big"
+            image={saloniImage}
+          />
+          <Suggestions />
+          <Footer />
+        </div>
+      </Sticky>
+    </div>
+  );
 }
 
 export default Sidebar;
